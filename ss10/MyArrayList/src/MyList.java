183e4a4817c882d23ed4809535940bd7f49f1a06
baitapGYM <tuongminh7889@gmail.com>
2023-05-21 15:37:38 +0700

    ok

--- ss10/MyArrayList/src/MyList.java
+++ ss10/MyArrayList/src/MyList.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 public class MyList<E> {
     private int size = 0;
     private static final int DEFAULT_CAPACITY = 10;
-    Object elements[];
+    Object[] elements;
 
     public MyList() {
         elements = new Object[DEFAULT_CAPACITY];
